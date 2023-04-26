c9a835b38f87173fdfdd9c3cab6c061e81d15c20
benjamin2orie <ogechukwubenj@gmail.com>
2023-04-26 14:04:21 -0700

    updating styling

--- src/Components/Meet.js
+++ src/Components/Meet.js
@@ -1,9 +1,10 @@
 // import { Form } from 'react-router-dom';
+import styles from './Meet.module.css';
 import logo from './logo.avif';
 function Meet() {
     return(
         
-        <header className = "head">
+        <header className = {styles.head}>
             <div className ="head">
                 <p>This is my react project project so as a front-end developers</p>
             </div>
